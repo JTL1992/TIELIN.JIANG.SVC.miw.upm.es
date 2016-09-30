883835a350f5bd970c86303351c3cc463621f98f
JTL1992 <talentjiang@yeah.net>
2016-10-01 01:49:16 +0200

    create C52Test

---
+++ src/test/java/es/upm/miw/SVC/tielin/jiang/C52Test.java
@@ -0,0 +1,15 @@
+package es.upm.miw.SVC.tielin.jiang;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class C52Test {
+
+    @Test
+    public void testMA() {
+        C52 c52 = new C52();
+        assertEquals("mA",c52.mA());
+    }
+
+}
