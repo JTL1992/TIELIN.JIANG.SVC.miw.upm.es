98f935c1f4c5dbb531cfa159a6f5afe6b98733ae
JTL1992 <talentjiang@yeah.net>
2016-10-01 00:51:39 +0200

    create C32 C32Test

---
+++ src/test/java/es/upm/miw/SVC/tielin/jiang/C32Test.java
@@ -0,0 +1,15 @@
+package es.upm.miw.SVC.tielin.jiang;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class C32Test {
+
+    @Test
+    public void testMA() {
+        C32 c32 = new C32();
+        assertEquals("mA",c32.mA());
+    }
+
+}
